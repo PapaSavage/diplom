8dbee641bc7a5052b58b1e970bee3a419c205039
Falaleev Danil <danilkafalaleev@gmail.com>
2025-05-01 20:52:17 +0300

    add registartion

--- frontend/pages/signup.vue
+++ frontend/pages/signup.vue
@@ -2,26 +2,37 @@
   <div class="h-screen flex flex-col justify-center items-center">
     <Card class="max-w-sm mx-4">
       <CardHeader>
-        <CardTitle class="text-xl"> Регистрация </CardTitle>
+        <CardTitle class="text-xl">Регистрация</CardTitle>
         <CardDescription>
           Введите свои данные для создания аккаунта
         </CardDescription>
       </CardHeader>
       <CardContent>
-        <div class="grid gap-4">
+        <form @submit.prevent="submitForm" class="grid gap-4">
           <div class="grid grid-cols-2 gap-4">
             <div class="grid gap-2">
               <Label for="first-name">Имя</Label>
-              <Input id="first-name" placeholder="Максим" required />
+              <Input
+                v-model="formData.firstName"
+                id="first-name"
+                placeholder="Максим"
+                required
+              />
             </div>
             <div class="grid gap-2">
               <Label for="last-name">Фамилия</Label>
-              <Input id="last-name" placeholder="Петров" required />
+              <Input
+                v-model="formData.lastName"
+                id="last-name"
+                placeholder="Петров"
+                required
+              />
             </div>
           </div>
           <div class="grid gap-2">
             <Label for="email">Email</Label>
             <Input
+              v-model="formData.email"
               id="email"
               type="email"
               placeholder="m@example.com"
@@ -30,17 +41,50 @@
           </div>
           <div class="grid gap-2">
             <Label for="password">Пароль</Label>
-            <Input id="password" type="password" />
+            <Input
+              v-model="formData.password"
+              id="password"
+              type="password"
+              required
+            />
           </div>
-          <Button type="submit" class="w-full"> Создать аккаунт </Button>
-        </div>
+          <Button type="submit" class="w-full">Создать аккаунт</Button>
+        </form>
         <div class="mt-4 text-center text-sm">
           Уже есть аккаунт?
-          <a href="#" class="underline">
-            <NuxtLink to="/login">Войти</NuxtLink>
-          </a>
+          <NuxtLink to="/login" class="underline">Войти</NuxtLink>
         </div>
       </CardContent>
     </Card>
   </div>
 </template>
+
+<script setup>
+import { ref } from "#imports";
+import { useFetch } from "#app";
+
+const formData = ref({
+  firstName: "",
+  lastName: "",
+  email: "",
+  password: "",
+});
+
+async function submitForm() {
+  try {
+    const response = await useFetch("http://127.0.0.1:8000/api/register", {
+      method: "POST",
+      body: JSON.stringify({
+        name: `${formData.value.firstName} ${formData.value.lastName}`,
+        email: formData.value.email,
+        password: formData.value.password,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  } catch (error) {
+    console.error(error);
+  }
+}
+</script>
